7041ac66fc184f7f2e2a0564dab6b7efa87118f8
Daphne <daphne@ntu.im>
2021-10-08 10:57:52 +0800

    hw2 done

---
+++ hw2/main.js
@@ -0,0 +1,57 @@
+const images = [
+  "https://static.wikia.nocookie.net/characters/images/7/7f/Tom_Cat.jpg/revision/latest?cb=20190404160415",
+  "http://2.bp.blogspot.com/-DLKuY8I_nAQ/UT3c_7_xHBI/AAAAAAAAWkU/tudk9Gk5Qn0/s1600/JERRY+23.jpg",
+  "https://cdn.pixabay.com/photo/2020/05/11/15/38/tom-5158824_1280.png",
+  "https://thumbs.dreamstime.com/b/tom-jerry-cartoon-white-background-tom-jerry-cartoon-white-background-226542613.jpg",
+];
+
+let currentIndex = 0;
+let album = 1;
+
+let curImage = document.getElementById("photo"),
+  prevButton = document.getElementById("prevBtn"),
+  nextButton = document.getElementById("nextBtn");
+
+setCurImage(currentIndex);
+
+function prevImg() {
+  if (currentIndex > 0) setCurImage(currentIndex - 1);
+}
+function nextImg() {
+  if (currentIndex < images.length - 1) setCurImage(currentIndex + 1);
+}
+function setCurImage(index) {
+  setSpecialEffect(index);
+  currentIndex = index;
+  setButtonState(index);
+  curImage.src = images[index];
+}
+function setButtonState() {
+  if (currentIndex === 0) {
+    prevButton.classList.add("disabled");
+  } else if (currentIndex === images.length - 1) {
+    nextButton.classList.add("disabled");
+  } else {
+    prevButton.classList.remove("disabled");
+    nextButton.classList.remove("disabled");
+  }
+}
+function setSpecialEffect(newIndex) {
+  previewImage = document.getElementById(`pic${currentIndex}`);
+  currentImage = document.getElementById(`pic${newIndex}`);
+  previewImage.classList.remove("selected");
+  currentImage.classList.add("selected");
+}
+
+function selectAlbum(n) {
+  if (n === 3) {
+    alert("Sorry, empty album!");
+  } else {
+    prevAlbum = document.getElementById(`album${album}`);
+    curAlbum = document.getElementById(`album${n}`);
+    prevAlbum.classList.remove("selectedAlbum");
+    curAlbum.classList.add("selectedAlbum");
+    album = n;
+    setCurImage(0);
+  }
+}
