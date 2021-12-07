351ca5ba54c6e476d21452145310f434134512a9
侯維書 <houweishu@Daphne-de-MacBook-Air.local>
2021-12-07 10:54:54 +0800

    hack2

---
+++ hack2/server/routes/post.js
@@ -0,0 +1,51 @@
+import express from "express";
+import Post from "../models/post";
+import moment from "moment";
+
+const router = express.Router();
+
+// TODO 2-(1): create the 1st API (/api/allPosts)
+router.get("/allPosts", async (_, res) => {
+  try {
+    const post = await Post.find({}).sort({ timestamp: -1 });
+    res.status(200).send({ message: "success", data: post });
+  } catch (error) {
+    res.status(403).send({ message: "error", data: null });
+  }
+});
+
+// TODO 3-(1): create the 2nd API (/api/postDetail)
+router.get("/postDetail", async (req, res) => {
+  const { pid } = req.query;
+  try {
+    const post = await Post.findOne({ postId: pid });
+    res.status(200).send({ message: "success", post: post });
+  } catch (error) {
+    res.status(403).send({ message: "error", post: null });
+  }
+});
+
+// TODO 4-(1): create the 3rd API (/api/newPost)
+router.post("/newPost", async (req, res) => {
+  const { postId, title, content, timestamp } = req.body;
+  try {
+    const newPost = await new Post({ postId, title, content, timestamp });
+    await newPost.save();
+    res.status(200).send({ message: "success" });
+  } catch (error) {
+    res.status(403).send({ message: "error" });
+  }
+});
+
+// TODO 5-(1): create the 4th API (/api/post)
+router.delete("/post", async (req, res) => {
+  const { pid } = req.query;
+  try {
+    await Post.deleteOne({ postId: pid });
+    res.status(200).send({ message: "success" });
+  } catch (error) {
+    res.status(403).send({ message: "error" });
+  }
+});
+
+export default router;
